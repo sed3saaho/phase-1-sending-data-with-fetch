ec898991597e31f0412bb971b57bc5b1c865a43a
Sedly <sedly.keith@student.moringaschool.com>
2024-04-04 13:57:38 +0300

    phase-1-sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData( name, email ) {
+    fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            usersname: name,
+            usersemail: email,
+        }),
+    } ).then( function ( response ) {
+        return response.json();
+    } ).then( function ( object ) {
+        console.log( object );
+    } ).catch( function ( error ) {
+        alert( "Bad things! Ragnarök!" );
+        console.log( error.message );
+    } );
+}
+// Testing our code
+submitData( "Bazenga", "Bazenga@Moringa.com" );.
